5f8f4984c86912937e5e9d784b6a9ba72a862806
KhovetS2 <isaque.elis.escola@gmail.com>
2024-03-18 02:59:49 -0300

    adicionando remaps e alterando a remap de refactor

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -101,6 +101,8 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format lua code
+			"prettier",
+			"prettierd",
 		})
 
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
