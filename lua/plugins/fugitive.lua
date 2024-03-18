3ef511d9b6123a5b179f39d8588130965f805b24
KhovetS2 <isaque.elis.escola@gmail.com>
2024-03-18 03:08:56 -0300

    adicionando remaps para o git

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,12 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		-- rebase always
+		vim.keymap.set("n", "<leader>gP", function()
+			vim.cmd.Git({ "pull", "--rebase" })
+		end, {})
+		vim.keymap.set("n", "<leader>gp", function()
+			vim.cmd.Git("push")
+		end, {})
 	end,
 }
